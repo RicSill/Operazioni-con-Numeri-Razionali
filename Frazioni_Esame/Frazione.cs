6b35e8ec714c2be70d32d9bc6794f297d8da55dc
ricsilano <ricsilano@gmail.com>
2022-02-16 17:37:40 +0100

    Aggiungere i file di progetto.

---
+++ Frazioni_Esame/Frazione.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazioni_Esame
+{
+    public class Frazione
+    {
+        public int Numeratore { get; set; }
+
+        public int Denominatore { get; set; }
+
+        //public double Value { get; set; }
+
+        public Frazione()
+        {
+            Numeratore = 0;
+            Denominatore = 1;
+            //Value = this.Numeratore / this.Denominatore;
+        }
+
+        public Frazione(int numeratore, int denominatore)
+        {
+            this.Numeratore = numeratore;
+            this.Denominatore = denominatore;
+            //this.Value = numeratore / denominatore;
+        }
+
+        public override string ToString()
+        {
+            return Numeratore + "/" + Denominatore;
+        }
+
+        public static Frazione Addizione(Frazione f1, Frazione f2)
+        {
+            Frazione addizione = new Frazione();
+            int num1, num2;
+            num1 = f1.Numeratore * f2.Denominatore;
+            num2 = f2.Numeratore * f1.Denominatore;
+            addizione.Numeratore = num1 + num2;
+            addizione.Denominatore = f1.Denominatore * f2.Denominatore;
+            addizione.Semplifica();
+            return addizione;
+        }
+
+        public static Frazione Sottrazione(Frazione f1, Frazione f2)
+        {
+            Frazione sottrazione = new Frazione();
+            int num1, num2;
+            num1 = f1.Numeratore * f2.Denominatore;
+            num2 = f2.Numeratore * f1.Denominatore;
+            sottrazione.Numeratore = num1 - num2;
+            sottrazione.Denominatore = f1.Denominatore * f2.Denominatore;
+            sottrazione.Semplifica();
+            return sottrazione;
+        }
+
+        public static Frazione Moltiplicazione(Frazione f1, Frazione f2)
+        {
+            Frazione moltiplicazione = new Frazione();
+            moltiplicazione.Numeratore = f1.Numeratore * f2.Numeratore;
+            moltiplicazione.Denominatore = f1.Denominatore * f2.Denominatore;
+            moltiplicazione.Semplifica();
+            return moltiplicazione;
+        }
+
+        public static Frazione Divisione(Frazione f1, Frazione f2)
+        {
+            Frazione divisione = new Frazione();
+            divisione.Numeratore = f1.Numeratore * f2.Denominatore;
+            divisione.Denominatore = f2.Numeratore * f1.Denominatore;
+            divisione.Semplifica();
+            return divisione;
+        }
+        public void Semplifica()
+        {
+            int gcd = 0;
+            for (int x = 1; x >= Denominatore; x++)
+            {
+                if ((Numeratore % x == 0) && (Denominatore % x == 0))
+                    gcd = x;
+            }
+            if (gcd != 0)
+            {
+                Numeratore = Numeratore / gcd;
+                Denominatore = Denominatore / gcd;
+            }
+        }
+
+
+    }
+}
---
+++ Frazioni_Esame/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace Frazioni_Esame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OPERAZIONE TRA 2 FRAZIONI\n");
+            Frazione f1 = new Frazione();
+            Frazione f2 = new Frazione();
+            Thread.Sleep(1500);
+            Console.WriteLine("Frazione 1:");
+            f1.Numeratore = int.Parse(Console.ReadLine());
+            Console.WriteLine("/");
+            f1.Denominatore = int.Parse(Console.ReadLine());
+            Console.WriteLine("Frazione 2:");
+            f2.Numeratore = int.Parse(Console.ReadLine());
+            Console.WriteLine("/");
+            f2.Denominatore = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Frazione 1: {f1.ToString()}\n");
+            Console.WriteLine($"Frazione 2: {f2.ToString()}\n\n");
+
+            Console.WriteLine("Cosa vuoi Fare? \n" +
+                "1. Addizione\n2. Sottrazione\n3. Montiplicazione\n4. Divisione\n9. Esci");
+            string i = Console.ReadLine();
+            switch(i)
+            {
+                case "1":
+                case "+":
+                    
+                    Console.WriteLine("\n{0} + {1} = {2}", f1.ToString(), f2.ToString(), Frazione.Addizione(f1, f2).ToString());
+                    Console.ReadLine();
+                    break;
+                case "2": 
+                case "-":
+                    Console.WriteLine("\n{0} -{1} = {2}", f1.ToString(), f2.ToString(), Frazione.Sottrazione(f1, f2).ToString());
+                    Console.ReadLine();
+                    break;
+                    
+                case "3":
+                case "x":
+                    Console.WriteLine("\n{0} x {1} = {2}", f1.ToString(), f2.ToString(), Frazione.Moltiplicazione(f1, f2).ToString());
+                    Console.ReadLine();
+                    break;
+
+                case "4":
+                case ":":
+                    Console.WriteLine("\n{0} : {1} = {2}", f1.ToString(), f2.ToString(), Frazione.Divisione(f1, f2).ToString());
+                    Console.ReadLine();
+                    break;
+                case "9":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+
+            }
+            
+         }
+    }
+}
